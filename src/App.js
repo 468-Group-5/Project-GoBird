a6227c96d0c8a9802091e200c735bef5f63718db
Cole Snyder <CS948810@WCUPA.EDU>
2024-03-26 09:10:51 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,12 +17,14 @@ function App() {
       <main className="main">
         <section className="hero">
           <h2>Welcome!</h2>
-          <p>-------------------------------</p>
+          <p>GoBird is a cutting-edge mobile application designed to simplify and streamline the parking process around West Chester University. Our app provides real-time information on available and taken parking spaces, ensuring that you can spend less time searching for a spot and more time focusing on what matters to you.</p>
           <button>Learn More</button>
         </section>
         <section className="content">
           <h3>Our Services</h3>
-          <p>GoBird is a cutting-edge mobile application designed to simplify and streamline the parking process around West Chester University. Our app provides real-time information on available and taken parking spaces, ensuring that you can spend less time searching for a spot and more time focusing on what matters to you.</p>
+          <p>Our team’s goal is to simplify parking on the West Chester University campus for students, faculty, and visitors by developing a web application that directs you to available parking spaces. GoBird is an attendance tracker for cars parked in West Chester University spaces; it will deliver real-time parking availability updates to the user in an easy-to-use interface. The user will have a clear image of what areas have availability, what areas do not, while using familiar mapping to get there.</p> 
+
+          <p>As users enter a parking area, they will be logged in our system – as they leave, they will be logged out. Using data crowdsourced from each user we can offer near real-time updates to each user on where they can or cannot park while also displaying this information on the Google Maps interface the users are familiar with. </p>
         </section>
       </main>
       <footer className="footer">
