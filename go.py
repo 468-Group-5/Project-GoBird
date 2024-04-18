13e598fd5919f9c9ddfd959dc6183d14c6d11c75
Bxdda (Chaughn) <123420597+bxdda@users.noreply.github.com>
2024-04-18 10:09:25 -0400

    Update go.py
    testing

--- go.py
+++ go.py
@@ -1,11 +1,6 @@
-from flask import Flask, request, jsonify
 import mysql.connector
-import requests
 
 #needs a function to constantly update currTime, to reference against parkingTimes saved in mysql
-
-# Initialize Flask app
-app = Flask(__name__)
 
 # Configure MySQL connection    --***THIS MUST BE UPDATED!!!***
 mysql_host = "localhost"
@@ -21,7 +16,5 @@ db = mysql.connector.connect(
 
 # Cursor for executing SQL queries
 cursor = db.cursor()
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)        #default IP matches any address, exposed port
 
 #Add endpoints for requests from React: 
