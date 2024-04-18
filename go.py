17c84de8a0bb6187f23f0da05281adf494ad62af
Bxdda (Chaughn) <123420597+bxdda@users.noreply.github.com>
2024-04-18 12:09:25 -0400

    Update go.py

--- go.py
+++ go.py
@@ -3,7 +3,7 @@ import mysql.connector
 #needs a function to constantly update currTime, to reference against parkingTimes saved in mysql
 
 # Configure MySQL connection    --***THIS MUST BE UPDATED!!!***
-mysql_host = "localhost"
+mysql_host = "172.17.0.1"
 mysql_user = "root"
 mysql_password = "strong_password"
 
