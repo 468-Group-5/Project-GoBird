6e8e11d88b4763e7b3b3c2dbdd8867231a6f7cf8
Bxdda (Chaughn) <123420597+bxdda@users.noreply.github.com>
2024-04-21 17:11:25 -0400

    Update go.py
    trying updated mysql credentials

--- go.py
+++ go.py
@@ -1,20 +1,21 @@
 import mysql.connector
 
-#needs a function to constantly update currTime, to reference against parkingTimes saved in mysql
-
-# Configure MySQL connection    --***THIS MUST BE UPDATED!!!***
-mysql_host = "172.17.0.1"
+# Configure MySQL connection
+mysql_host = "mysql"  # Use the service name for MySQL within Kubernetes
 mysql_user = "root"
-mysql_password = "strong_password"
+mysql_password = "password"
+#mysql_database = "your_database_name"  # Specify your MySQL database name
 
 # Connect to MySQL database
 db = mysql.connector.connect(
     host=mysql_host,
     user=mysql_user,
     password=mysql_password,
+    #database=mysql_database
 )
 
 # Cursor for executing SQL queries
 cursor = db.cursor()
 
-#Add endpoints for requests from React: 
+# Add endpoints for requests from React:
+# Add your endpoint functions here
