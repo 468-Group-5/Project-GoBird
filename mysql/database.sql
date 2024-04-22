6894f86b01c9ab77217889946fd63562044814da
lxphung <157526554+lxphung@users.noreply.github.com>
2024-04-22 11:14:27 -0400

    Update database.sql

--- mysql/database.sql
+++ mysql/database.sql
@@ -26,5 +26,9 @@ CREATE TABLE spots (
 INSERT INTO customer(cust_username, cust_password, cust_plate)
 VALUES("user1", "pass1", "LYD876"), ("user2", "pass2", "YDK3842");
 
+CREATE USER 'root'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON gobird.* TO 'root'@'%';
+FLUSH PRIVILEGES;
+
 INSERT INTO spots(spots_number, spots_street, spots_status)
 VALUES("1", "S Church", "open"), ("2", "S Church", "open");
