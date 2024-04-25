7501b46e52fa02bc687478abb4528e44318e7e6c
lxphung <157526554+lxphung@users.noreply.github.com>
2024-04-25 09:41:05 -0400

    Update database.sql

--- mysql/database.sql
+++ mysql/database.sql
@@ -1,21 +1,6 @@
 CREATE DATABASE gobird;
 use gobird;
 
-CREATE TABLE customer (
-  cust_username varchar(50) NOT NULL,
-  cust_password varchar(50) NOT NULL,
-  cust_plate varchar(50) NOT NULL,
-  PRIMARY KEY (cust_username)
-);
-
-CREATE TABLE parking (
-  park_username varchar(50) NOT NULL,
-  park_spot_number varchar(50) NOT NULL,
-  park_time_in varchar(50) NOT NULL,
-  park_time_out int,
-  PRIMARY KEY (park_username)
-);
-
 CREATE TABLE spots (
   spots_number int NOT NULL,
   spots_street varchar(50) NOT NULL,
@@ -23,11 +8,17 @@ CREATE TABLE spots (
   PRIMARY KEY (spots_number)
 );
 
-INSERT INTO customer(cust_username, cust_password, cust_plate)
-VALUES("user1", "pass1", "LYD876"), ("user2", "pass2", "YDK3842");
+CREATE TABLE parking (
+  park_spot_number int NOT NULL,
+  park_time_in varchar(50) NOT NULL,
+  park_time_out varchar(50) NOT NULL,
+  PRIMARY KEY (park_spot_number),
+  FOREIGN KEY (park_spot_number) REFERENCES spots(spots_number)
+);
 
-INSERT INTO spots(spots_number, spots_street, spots_status)
-VALUES("1", "S Church", "open"), ("2", "S Church", "open");
+INSERT INTO spots (spots_number, spots_street, spots_status)
+VALUES ("1", "S Church", "open"), ("2", "S Church", "taken"),("3", "S Church", "taken");
+
 
 CREATE USER 'root'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON gobird.* TO 'root'@'%';
