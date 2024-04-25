e1ad4a6766470439db367bff8598398d8e3126c0
Justin-Linwood <89924129+Justin-Linwood@users.noreply.github.com>
2024-04-25 08:30:02 -0400

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
-import { useMemo } from "react";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import React, { useMemo, useEffect, useState } from "react";
+import { GoogleMap, useLoadScript, Marker, OverlayView } from "@react-google-maps/api";
 
 export default function Home() {
   const apiKey = process.env.API_KEY
@@ -11,12 +11,72 @@ export default function Home() {
   return <Map />;
 }
 
+function getLocation(setCenter) {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+        setCenter({ lat, lng });
+      },
+      (error) => {
+        switch(error.code) {
+          case error.PERMISSION_DENIED:
+            alert("User denied the request for Geolocation.");
+            break;
+          case error.POSITION_UNAVAILABLE:
+            alert("Location information is unavailable.");
+            break;
+          case error.TIMEOUT:
+            alert("The request to get user location timed out.");
+            break;
+          case error.UNKNOWN_ERROR:
+            alert("An unknown error occurred.");
+            break;
+          default:
+            alert("An error occurred while getting user location.");
+        }
+      }
+    );
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+}
+
 function Map() {
-  const center = useMemo(() => ({ lat: 39.9524, lng: -75.5981 }), []);
+  const [center, setCenter] = useState(null);
+  const overlayPosition = useMemo(() => ({ lat: 39.9524, lng: -75.5981 }), []);
+
+  const getPixelPositionOffset = (width, height) => ({
+    x: -(width / 2),
+    y: -(height / 2),
+  });
+
+  useEffect(() => {
+    getLocation(setCenter);
+  }, []);
+
+  if (!center) return <div>Loading...</div>;
 
   return (
-    <GoogleMap zoom={18} center={center} mapContainerClassName="map-container">
+    <GoogleMap
+      id="map"
+      mapContainerStyle={{ width: "100%", height: "600px" }}
+      zoom={18}
+      center={center}
+    >
       <Marker position={center} />
+      <OverlayView
+        position={overlayPosition}
+        mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+        getPixelPositionOffset={getPixelPositionOffset}
+      >
+        <img
+          src="green.png"
+          alt="Overlay Image"
+          style={{ width: "100px", height: "100px" }} // Adjust the size as needed
+        />
+      </OverlayView>
     </GoogleMap>
   );
 }
